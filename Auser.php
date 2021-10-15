e1d2bb7d3d10d01697be7be8fe19a64c78c19d74
Euphonium <60012994+AkinaSinon@users.noreply.github.com>
2021-10-16 00:40:02 +0800

    Update Auser.php

--- Auser.php
+++ Auser.php
@@ -12,17 +12,7 @@ class Auser extends Controller
     // Admin登录管理功能模块
     public function index()
     {
-        //$request = Request::instance();
-        //dump($request);
-        // echo "当前域名是:".$request->domain().'<br />';
-        // echo "当前入口文件是:".$request->baseFile().'<br />';
-        // echo "当前后缀是:".$request->ext().'<br />';
-        // echo "当前操作是:".$request->action().'<br />';
-        // echo "当前请求方法是:".$request->method().'<br />';
     	return $this->fetch('');
-        //$result = Db::execute('insert into bd_admin (username,password) value("Tester","123456")');
-        //$result = Db::execute('insert into bd_admin (username,password) value("Tester","123456")');
-        //dump($result);
     }
     
     public function showyzm()
@@ -42,8 +32,6 @@ class Auser extends Controller
             $info = Request::instance()->post();
             $username = $info['username'];
             $password = $info['password'];
-            //$auser = new Auser();
-            //$result = $username->getUser(username,md5($password));
             $res = Db::table('bd_admin')->where('username',input('post.username'))->select();
             if (!$res) 
             {
@@ -88,9 +76,6 @@ class Auser extends Controller
             $admin = input('post.');
             //写入数据库
             $res = Db::table('bd_admin')->insert($admin);
-            // $this->assign('res',$res);
-            // dump($res);
-            // die;
             if ($res) {
                 return $this->success('管理账号注册成功(≧∇≦)/',Url('admin/Index/addAdn'));
             }else{
@@ -100,4 +85,4 @@ class Auser extends Controller
     }
 
 }
-
+?>
