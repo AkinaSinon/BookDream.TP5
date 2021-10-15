bf00277f2273a133be20b2d81bd7d3d14c1fa7f1
Euphonium <60012994+AkinaSinon@users.noreply.github.com>
2021-10-16 00:38:10 +0800

    Create Base.php

---
+++ Base.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\Admin\controller;
+use think\Controller;
+class Base extends Controller
+{
+
+  /*删除功能*/
+  public function del($id)
+  {
+   $id = input("get.id");
+   //删除语句
+   $res = model("category")->del($id);
+   if($res)
+   {
+     $this->success("删除成功");
+   }else
+   {
+    $this->error("删除失败");
+   }
+
+  }
+?>
